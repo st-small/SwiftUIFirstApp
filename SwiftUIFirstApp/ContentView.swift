2640508f15591768129a044ba3233360fdd8c68b
Станислав Шияновский <st-small@rambler.ru>
2019-11-03 13:35:14 +0200

    Created table view with navigation logic.

--- SwiftUIFirstApp/ContentView.swift
+++ SwiftUIFirstApp/ContentView.swift
@@ -8,14 +8,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+public struct Novel {
+    public var name: String
+    public var author: String
+    public var rating: String
+}
+
+public struct ContentView: View {
+    
+    public var novels = [
+        Novel(name: "Войн и мир", author: "Лев Толстой", rating: "B"),
+        Novel(name: "Мастер и Маргарита", author: "Михаил Булгаков", rating: "A")
+    ]
+    
+    public var body: some View {
+        NavigationView {
+            List {
+                ForEach(novels, id: \.name) { novel in
+                    NavigationLink(destination: Text(novel.author)) {
+                        Text(novel.rating)
+                            .font(.headline)
+                        Text(novel.name)
+                    }
+                }
+            }.navigationBarTitle("Романы")
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+public struct ContentView_Previews: PreviewProvider {
+    public static var previews: some View {
         ContentView()
     }
 }
