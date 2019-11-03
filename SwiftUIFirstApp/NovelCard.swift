a77bedb3ed9f670a6c4646c8b8f316870f98ec57
Станислав Шияновский <st-small@rambler.ru>
2019-11-03 16:39:33 +0200

    Added gestures to handle the novel card.

--- SwiftUIFirstApp/NovelCard.swift
+++ SwiftUIFirstApp/NovelCard.swift
@@ -9,29 +9,42 @@
 import SwiftUI
 
 struct NovelCard: View {
+    @Environment(\.managedObjectContext) var moc
     @State var randomNumber = Int.random(in: 1...4)
+    @State var showingAuthor = false
+    @State var dragAmount = CGSize.zero
     var novel: Novel
     
     var body: some View {
         VStack {
-            VStack {
-                Image("\(self.randomNumber)").resizable()
-                .frame(width: 300.0, height: 150.0)
-                Text(self.novel.name)
-                    .font(.largeTitle)
-                    .lineLimit(10)
-                    .padding([.horizontal])
-                
-                Text(self.novel.author)
-                    .font(.title)
-                    .lineLimit(10)
-                    .padding([.horizontal, .bottom])
+            GeometryReader { geo in
+                VStack {
+                    Image("\(self.randomNumber)").resizable()
+                        .frame(width: 300.0, height: 150.0)
+                    Text(self.novel.name)
+                        .font(.largeTitle)
+                        .lineLimit(10)
+                        .padding([.horizontal])
+                    
+                    Text(self.novel.author)
+                        .font(.title)
+                        .lineLimit(10)
+                        .padding([.horizontal, .bottom])
+                        .blur(radius: self.showingAuthor ? 0 : 6)
+                        .opacity(self.showingAuthor ? 1 : 0.25)
+                }
+                .multilineTextAlignment(.center)
+                .background(
+                    Color.white
+                        .shadow(color: .black, radius: 5)
+                )
+                .onTapGesture {
+                    withAnimation {
+                        self.showingAuthor.toggle()
+                    }
+                }
+                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).minX) / 10), axis: (x: 0, y: 1, z: 0))
             }
-            .multilineTextAlignment(.center)
-            .background(
-                Color.white
-                    .shadow(color: .black, radius: 5)
-            )
             
             Text(novel.rating)
                 .font(.system(size: 48))
@@ -39,5 +52,23 @@ struct NovelCard: View {
         }
         .frame(minHeight: 0, maxHeight: .infinity)
         .frame(width: 300)
+        .offset(y: self.dragAmount.height)
+        .gesture(
+            DragGesture()
+                .onChanged { self.dragAmount = $0.translation }
+                .onEnded { value in
+                    if self.dragAmount.height < -200 {
+                        self.dragAmount = CGSize(width: 0, height: -1000)
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            self.moc.delete(self.novel)
+                            //                            try? self.moc.save()
+                        }
+                    } else {
+                        self.dragAmount = .zero
+                    }
+            }
+        )
+            .animation(.spring())
     }
 }
