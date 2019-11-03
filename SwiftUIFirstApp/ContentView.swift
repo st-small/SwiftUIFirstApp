10f6abcef8338192a3ddf355dcc8cc32461a64fd
Станислав Шияновский <st-small@rambler.ru>
2019-11-03 13:02:10 +0200

    Initial Commit

---
+++ SwiftUIFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirstApp
+//
+//  Created by Станислав Шияновский on 11/3/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
